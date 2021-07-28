e37c9b57a002f7cd5fc92c7ad71122172ba48481
Fleshner <86986665+Fleshner@users.noreply.github.com>
2021-07-28 09:34:25 -0700

    Incorporation of csv writing and pulls from list/keys

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 #First, ensure you create a clone of this repo and name it accordingly. 
+#Once you clone the rep, open up the file from your Desktop software using the below code in your terminal/command-line:
+
+''' cd ~/Desktop/robo-advisor
+
+#After repo is opened, make the instructions for the service by creating a separate "robo_advisor.py" file in the Desktop app you're using. This will be used to ensure your set-up and environment are correct.
+
+'''
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
+'''
+
+
+#We then need to create a requirements file to ensure all packages are downloaded that we need.
+
+"requests
+python-dotenv"
+
+#Note: You can use pandas if you'd like above, but just ensure to enable it in the code above.
+
+#Now, create/set up the environment:
+
+'''
+conda create -n stocks-env python=3.8 # (first time only)
+conda activate stocks-env
+"
+
+#Install your packakes by activating pip install requirements...
+
+#now test the system. If this processes, we're good to continue:
+"
+python app/robo_advisor.py
+"
+
+
+
+
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,7 +1,7 @@
 # this is the "app/robo_advisor.py" file
 # this is the "app/robo_advisor.py" file
+import json
 import re
-
 import requests
 print("-------------------------")
 x = input("Select your symbol:")
@@ -26,10 +26,56 @@ load_dotenv()
 api = os.getenv("api_key")
 stock_symbol = x
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED'
 final_url = f'{url}&symbol={stock_symbol}&apikey={api}'
 
-r = requests.get(final_url)
-data = r.json()
+import pandas as pd
+import csv
 
-print(data)
+r = requests.get(final_url)
+parsed_response = json.loads(r.text)
+
+#Got to this point before I needed to consult with the screencast for some added guidance. Thank you!
+
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd)
+
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+dates = list(tsd.keys()) 
+
+latest_day = dates[0] #for the latest day
+latest_close = tsd[latest_day]["4. close"] #what was the last close?
+
+high_prices = []
+low_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
+    high_prices.append(float(high_price)) #converted string
+    low_prices.append(float(low_price))
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+#
+# INFO OUTPUTS
+#
+csv_file_path = "data/prices.csv"
+csv_headers = ["timestamp", "open", "high", "low", "close"]
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+        })
